6230588e936e47eb4862a2520fa35cd8978458ac
mika1407 <koneautomaatio@hotmail.com>
2021-04-23 21:55:17 +0300

    last push

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, {useEffect, useState} from 'react';
+import {render} from 'react-dom';
+import axios from 'axios';
+import "./App.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const CurrencyConverter = () => {
+  const [uSDAUD, setUSDAUD] = useState("");
+  const [first, setFirst] = useState("EUR");
+  const [second, setSecond] = useState("USD");
+  const [rate, setRate] = useState([]);
+
+
+  const getRate = (first, second) => {
+    axios({
+      method: "GET",
+      url:
+      `https://free.currconv.com/api/v7/convert?q=${first}_${second}&compact=ultra&apiKey=8ff18dd74666aa1f262c`,
+    })
+    .then((response) => {
+      console.log(response.data)
+      setRate(response.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  };
+
+  return (
+      <div >
+      <div className="otsikko">
+        Currency Converter
+      </div>
+      <div className="alapalkki"></div>
+      <br />
+    <div style={{ marginLeft: "33%"}}>
+      <div className="nakyma">
+          1 {first} = {rate[`${first}_${second}`]} {second}
+      </div>
+      <br />
+      <input className="input1"  type="text" value={first} onChange={(e) => setFirst(e.target.value)} />
+      <input className="input1"  type="text" value={second} onChange={(e) => setSecond(e.target.value)}/>
+      <button className="button"  onClick={() => {
+        getRate(first, second);
+      }}>Convert</button>
+      <br/>
+      <p style={{ color: "#333", fontWeight: "bold", fontSize: "25px" }}>Currencys:</p>
+      <ol style={{ color: "#333", fontSize: "20px" }}>  
+        <li>USD = US dollar</li>
+        <li>EUR = Euro</li>
+        <li>AUD = Australian dollar</li>
+        <li>CNY = Chinese Yuan</li>
+        <li>CHF = Swiss Franc</li>
+        <li>JPY = Japanese Yen</li>
+      </ol>
+      <br/>
+    </div>
+    </div>
+  )
+}
+
+render(<CurrencyConverter />, document.querySelector("#root"));
+
